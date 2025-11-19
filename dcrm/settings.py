754fb11aae97cbc9b95698542544f7219f648768
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-11-19 13:58:48 +0000

    .

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.app.github.dev",
+    "https://localhost:8000",
+]
--- website/views.py
+++ website/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, 'You have been logged in!')
+        else:
+            messages.success(request, 'There was an error, please login again...')
+        return redirect('home')
     return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
+# def login_user(request):
+#     pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
